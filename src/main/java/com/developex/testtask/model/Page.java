535648fc825c8b3f69439f88c66aab75bf8cd017
logan <logan@rocke.tech>
2020-11-12 16:47:38 +0200

    add Page and SearchSession models

--- src/main/java/com/developex/testtask/model/Page.java
+++ src/main/java/com/developex/testtask/model/Page.java
@@ -1,4 +1,29 @@
 package com.developex.testtask.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+@Data
+@Entity
+@Table(name = "pages")
+@Accessors(chain = true)
 public class Page {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @Column
+    private String url;
+
+    @Column
+    private String status;
+
+    @Column(name = "error_message")
+    private String errorMessage;
 }
