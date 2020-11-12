c70da071b5dc3ca31fcbf05e8d9310e18dcc9ee2
logan <logan@rocke.tech>
2020-11-12 16:56:13 +0200

    add sql init script

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -2,7 +2,8 @@ DROP TABLE IF EXISTS pages;
 
 CREATE TABLE pages
 (
-    id     INT AUTO_INCREMENT PRIMARY KEY,
-    url    VARCHAR(250) NOT NULL,
-    status VARCHAR(250) NOT NULL
+    id            INT AUTO_INCREMENT PRIMARY KEY,
+    url           VARCHAR(250) NOT NULL,
+    status        VARCHAR(250) NOT NULL,
+    error_message VARCHAR(250) NULL_TO_DEFAULT
 );
